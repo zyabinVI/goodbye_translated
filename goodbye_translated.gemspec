db52ec178175310b00810d5b5093d665b616ae8b
Andrew Zyabin <schastlilya@gmail.com>
2016-05-26 14:14:02 +0400

    Break out the `Translator` into a separate file

--- goodbye_translated.gemspec
+++ goodbye_translated.gemspec
@@ -1,12 +1,12 @@
 Gem::Specification.new do |s|
   s.name        = 'goodbye_translated'
-  s.version     = '0.0.1'
+  s.version     = '0.1.0'
   s.date        = '2016-05-25'
   s.summary     = "Saying goodbye to anything and everything, goodbye_translated neglects none!"
   s.description = "A simple goodbye gem, that says goodbye, with features for saying goodbye to different people in different languages."
   s.authors     = ["Andrew Zyabin"]
   s.email       = 'schastlilya@gmail.com'
-  s.files       = ["lib/goodbye_translated.rb"]
+  s.files       = ["lib/goodbye_translated.rb", "lib/goodbye_translated/translator.rb"]
   s.homepage    =
     'https://github.com/zyabinVI/goodbye_translated'
   s.license       = 'MIT'
--- lib/goodbye_translated.rb
+++ lib/goodbye_translated.rb
@@ -29,39 +29,4 @@ class GoodbyeTranslated
   end
 end
 
-class GoodbyeTranslated::Translator
-  def initialize(language)
-    @language = language
-  end
-
-  def say_goodbye
-    case @language
-      when "spanish"
-        return "Adios, Mundo!"
-      else # Assume english
-        return "Goodbye, World!"
-    end
-  end
-
-  def say_goodbye_to(people)
-    case @language
-      when "spanish"
-        if people.nil?
-          return say_goodbye
-        elsif people.respond_to?("join")
-          # Join the list elements with commas
-          return "Adios, #{people.join(", ")}!"
-        else
-          return "Adios, #{people}!"
-        end
-      else
-        if people.nil?
-          return say_goodbye
-        elsif people.respond_to?("join")
-          return "Goodbye, #{people.join(", ")}!"
-        else
-          return "Goodbye, #{people}!"
-        end
-    end
-  end
-end
+require 'goodbye_translated/translator'
