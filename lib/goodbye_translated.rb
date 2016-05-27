aea6eccebf15997e161ed0a28c64f08933ec9d0d
Andrew Zyabin <schastlilya@gmail.com>
2016-05-27 14:19:24 +0400

    Add documentation (a **small** amount...)

--- lib/goodbye_translated.rb
+++ lib/goodbye_translated.rb
@@ -1,8 +1,11 @@
+# The main goodbye_translated driver
 class GoodbyeTranslated
+  # Say goodbye to the world!
   def self.say_goodbye
     return "Goodbye, World!"
   end
   
+  # Say goodbye to the world, a person or a list of people.
   def self.say_goodbye_to(people)
     if people.nil?
       return self.say_goodbye
@@ -14,16 +17,19 @@ class GoodbyeTranslated
     end
   end
 
+  # Say goodbye to the world in a language.
   def self.say_goodbye_in(language)
     translator = Translator.new(language)
     return translator.say_goodbye
   end
 
+  # Say goodbye to some people in a language.
   def self.say_goodbye_to_in(people, language)
     translator = Translator.new(language)
     return translator.say_goodbye_to(people)
   end
 
+  # Say goodbye in a language to some people.
   def self.say_goodbye_in_to(language, people)
     return self.say_goodbye_to_in(people, language)
   end
